b8265f33ede8194735114c886376e1afc8f847dc
fulleni <fulleni@hotmail.com>
2025-03-11 12:29:18 +0100

    chore: initial commit
    - Added project structure
    - Added basic workflows
    - Added analysis options

---
+++ android/app/src/main/kotlin/headlines/toolkit/github/io/ht/main/MainActivity.kt
@@ -0,0 +1,6 @@
+package headlines_toolkit.github.io.ht_main
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:ht_main/app/app.dart';
+import 'package:ht_main/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}

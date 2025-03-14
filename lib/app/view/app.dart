698c9f42ccc2c9d010ece581d79713d3aa425637
fulleni <fulleni@hotmail.com>
2025-03-14 07:20:22 +0100

    feat: inject repository into app
    - Inject HtHeadlinesRepository
    - Use MultiRepositoryProvider
    - Use MultiBlocProvider

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -2,9 +2,53 @@ import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:ht_headlines_repository/ht_headlines_repository.dart';
 import 'package:ht_main/app/bloc/app_bloc.dart';
 import 'package:ht_main/l10n/l10n.dart';
 import 'package:ht_main/router/router.dart';
+
+class AppPage extends StatelessWidget {
+  const AppPage({
+    required HtHeadlinesRepository htHeadlinesRepository,
+    super.key,
+  }) : _htHeadlinesRepository = htHeadlinesRepository;
+
+  final HtHeadlinesRepository _htHeadlinesRepository;
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiRepositoryProvider(
+      providers: [RepositoryProvider.value(value: _htHeadlinesRepository)],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(
+            create: (context) => AppBloc(),
+          ),
+        ],
+        child: const _AppView(),
+      ),
+    );
+  }
+}
+
+class _AppView extends StatelessWidget {
+  const _AppView();
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<AppBloc, AppState>(
+      builder: (context, state) {
+        return MaterialApp.router(
+          theme:
+              state.themeMode == ThemeMode.light ? lightTheme() : darkTheme(),
+          routerConfig: appRouter,
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+        );
+      },
+    );
+  }
+}
 
 ThemeData lightTheme() {
   return FlexThemeData.light(
@@ -18,26 +62,4 @@ ThemeData darkTheme() {
     scheme: FlexScheme.material,
     fontFamily: GoogleFonts.notoSans().fontFamily,
   );
-}
-
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => AppBloc(),
-      child: BlocBuilder<AppBloc, AppState>(
-        builder: (context, state) {
-          return MaterialApp.router(
-            theme:
-                state.themeMode == ThemeMode.light ? lightTheme() : darkTheme(),
-            routerConfig: appRouter,
-            localizationsDelegates: AppLocalizations.localizationsDelegates,
-            supportedLocales: AppLocalizations.supportedLocales,
-          );
-        },
-      ),
-    );
-  }
 }
--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,6 +1,10 @@
+import 'package:ht_headlines_inmemory/ht_headlines_inmemory.dart';
+import 'package:ht_headlines_repository/ht_headlines_repository.dart';
 import 'package:ht_main/app/app.dart';
 import 'package:ht_main/bootstrap.dart';
 
 void main() {
-  bootstrap(() => const App());
+  final headlinesClient = HtInMemoryHeadlinesClient();
+  final headlinesRepository = HtHeadlinesRepository(client: headlinesClient);
+  bootstrap(() => AppPage(htHeadlinesRepository: headlinesRepository));
 }

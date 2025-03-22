bc56561f90532899b9c05f07e6e874bc08ab226c
fulleni <fulleni@hotmail.com>
2025-03-22 08:14:52 +0100

    feat: integrate Firebase Firestore
    - Use Firestore as data source
    - Replaced in-memory client
    - Added Firebase initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:ht_headlines_inmemory/ht_headlines_inmemory.dart';
+import 'package:ht_headlines_firestore/ht_headlines_firestore.dart';
 import 'package:ht_headlines_repository/ht_headlines_repository.dart';
 import 'package:ht_main/app/app.dart';
 import 'package:ht_main/bloc_observer.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   Bloc.observer = const AppBlocObserver();
-
-  final headlinesClient = HtInMemoryHeadlinesClient();
+  final firestore = FirebaseFirestore.instance;
+  final headlinesClient = HtHeadlinesFirestore(firestore: firestore);
   final headlinesRepository = HtHeadlinesRepository(client: headlinesClient);
 
   runApp(App(htHeadlinesRepository: headlinesRepository));

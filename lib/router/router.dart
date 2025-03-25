3d4a7140bc516ba84f3739691b587a3f27db5c03
fulleni <fulleni@hotmail.com>
2025-03-25 08:00:11 +0100

    refactor: remove AdaptiveScaffold
    - Simplifies routing
    - Removes unused dependency

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:ht_main/app/bloc/app_bloc.dart';
-import 'package:ht_main/app/view/app_scaffold.dart';
 import 'package:ht_main/authentication/view/authentication_page.dart';
 import 'package:ht_main/headline-details/view/headline_details_page.dart';
 import 'package:ht_main/headlines-feed/view/headlines_feed_page.dart';
@@ -13,34 +12,57 @@ final appRouter = GoRouter(
   initialLocation: Routes.headlinesFeed,
   redirect: (BuildContext context, GoRouterState state) {
     final appStatus = context.read<AppBloc>().state.status;
-    if (appStatus == AppStatus.authenticated) {
+    if (appStatus != AppStatus.authenticated) {
       return Routes.headlinesFeed;
     } else {
       return Routes.authentication;
     }
   },
   routes: [
-    ShellRoute(
-      builder: (context, state, child) {
-        return AppScaffold(child: child);
+    GoRoute(
+      path: Routes.authentication,
+      name: Routes.authenticationName,
+      builder: (BuildContext context, GoRouterState state) {
+        return const AuthenticationPage();
       },
       routes: [
         GoRoute(
-          path: Routes.headlinesFeed,
-          name: Routes.headlinesFeedName,
+          path: Routes.forgotPassword,
+          name: Routes.forgotPasswordName,
           builder: (BuildContext context, GoRouterState state) {
-            return const HeadlinesFeedPage();
+            return const Placeholder(child: Text('Forgot Password Page'));
           },
-          routes: [
-            GoRoute(
-              path: 'article/:id',
-              name: Routes.articleDetailsName,
-              builder: (BuildContext context, GoRouterState state) {
-                final id = state.pathParameters['id']!;
-                return HeadlineDetailsPage(headlineId: id);
-              },
-            ),
-          ],
+        ),
+        GoRoute(
+          path: Routes.resetPassword,
+          name: Routes.resetPasswordName,
+          builder: (BuildContext context, GoRouterState state) {
+            return const Placeholder(child: Text('Reset Password Page'));
+          },
+        ),
+        GoRoute(
+          path: Routes.confirmEmail,
+          name: Routes.confirmEmailName,
+          builder: (BuildContext context, GoRouterState state) {
+            return const Placeholder(child: Text('Confirm Email Page'));
+          },
+        ),
+      ],
+    ),
+    GoRoute(
+      path: Routes.headlinesFeed,
+      name: Routes.headlinesFeedName,
+      builder: (BuildContext context, GoRouterState state) {
+        return const HeadlinesFeedPage();
+      },
+      routes: [
+        GoRoute(
+          path: 'article/:id',
+          name: Routes.articleDetailsName,
+          builder: (BuildContext context, GoRouterState state) {
+            final id = state.pathParameters['id']!;
+            return HeadlineDetailsPage(headlineId: id);
+          },
         ),
         GoRoute(
           path: Routes.search,
@@ -57,36 +79,6 @@ final appRouter = GoRouter(
               child: Center(child: Text('SETTINGS PAGE')),
             );
           },
-        ),
-        GoRoute(
-          path: Routes.authentication,
-          name: Routes.authenticationName,
-          builder: (BuildContext context, GoRouterState state) {
-            return const AuthenticationPage();
-          },
-          routes: [
-            GoRoute(
-              path: Routes.forgotPassword,
-              name: Routes.forgotPasswordName,
-              builder: (BuildContext context, GoRouterState state) {
-                return const Placeholder(child: Text('Forgot Password Page'));
-              },
-            ),
-            GoRoute(
-              path: Routes.resetPassword,
-              name: Routes.resetPasswordName,
-              builder: (BuildContext context, GoRouterState state) {
-                return const Placeholder(child: Text('Reset Password Page'));
-              },
-            ),
-            GoRoute(
-              path: Routes.confirmEmail,
-              name: Routes.confirmEmailName,
-              builder: (BuildContext context, GoRouterState state) {
-                return const Placeholder(child: Text('Confirm Email Page'));
-              },
-            ),
-          ],
         ),
       ],
     ),

e0bd142ee07a00faac5a9049d97ed4fe05502b18
fulleni <fulleni@hotmail.com>
2025-05-25 19:16:01 +0100

    feat(account): load user content preferences
    - Load prefs on user change
    - Handle loading and error states
    - Use HtDataRepository

--- lib/account/bloc/account_state.dart
+++ lib/account/bloc/account_state.dart
@@ -1,26 +1,59 @@
 part of 'account_bloc.dart';
 
-/// Enum representing the status of the Account feature.
-enum AccountStatus { initial, loading, success, failure }
+/// Defines the status of the account state.
+enum AccountStatus {
+  /// The initial state.
+  initial,
 
-/// Represents the state of the Account feature.
-class AccountState extends Equatable {
-  const AccountState({this.status = AccountStatus.initial, this.errorMessage});
+  /// An operation is in progress.
+  loading,
 
-  /// The current status of the account feature operations.
+  /// An operation was successful.
+  success,
+
+  /// An operation failed.
+  failure,
+}
+
+/// {@template account_state}
+/// State for the Account feature.
+/// {@endtemplate}
+final class AccountState extends Equatable {
+  /// {@macro account_state}
+  const AccountState({
+    this.status = AccountStatus.initial,
+    this.user,
+    this.preferences,
+    this.errorMessage,
+  });
+
+  /// The current status of the account state.
   final AccountStatus status;
 
-  /// An optional error message if an operation failed.
+  /// The currently authenticated user.
+  final User? user;
+
+  /// The user's content preferences.
+  final UserContentPreferences? preferences;
+
+  /// An error message if an operation failed.
   final String? errorMessage;
 
-  /// Creates a copy of the current state with updated values.
-  AccountState copyWith({AccountStatus? status, String? errorMessage}) {
+  /// Creates a copy of this [AccountState] with the given fields replaced.
+  AccountState copyWith({
+    AccountStatus? status,
+    User? user,
+    UserContentPreferences? preferences,
+    String? errorMessage,
+  }) {
     return AccountState(
       status: status ?? this.status,
+      user: user ?? this.user,
+      preferences: preferences ?? this.preferences,
       errorMessage: errorMessage ?? this.errorMessage,
     );
   }
 
   @override
-  List<Object?> get props => [status, errorMessage];
+  List<Object?> get props => [status, user, preferences, errorMessage];
 }

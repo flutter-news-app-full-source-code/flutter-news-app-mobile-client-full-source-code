4cebbf32c8ae137bf970575a9fed336bffef7b8e
fulleni <fulleni@hotmail.com>
2025-05-31 07:18:59 +0100

    feat: remove headline details bloc
    - Removed unused files
    - Removed related dependencies

--- lib/headline-details/bloc/headline_details_event.dart
+++
@@ -1,9 +0,0 @@
-part of 'headline_details_bloc.dart';
-
-abstract class HeadlineDetailsEvent {}
-
-class HeadlineDetailsRequested extends HeadlineDetailsEvent {
-  HeadlineDetailsRequested({required this.headlineId});
-
-  final String headlineId;
-}

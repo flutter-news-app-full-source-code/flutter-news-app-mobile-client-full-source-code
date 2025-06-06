b8ae1ed03e9610cc475088da5faef04390f5cc1a
fulleni <fulleni@hotmail.com>
2025-06-06 06:35:16 +0100

    refactor: Simplify environment config selection

--- lib/main.dart
+++ lib/main.dart
@@ -4,17 +4,13 @@ import 'package:ht_main/bootstrap.dart';
 // Define the current application environment here.
 // Change this value to switch between environments for local development.
 const app_config.AppEnvironment currentEnvironment =
-    app_config
-        .AppEnvironment
-        .developmentInMemory; // Or .developmentApi, or .production
+    app_config.AppEnvironment.development; // production/development/demo
 
 void main() async {
   final appConfig = switch (currentEnvironment) {
     app_config.AppEnvironment.production => app_config.AppConfig.production(),
-    app_config.AppEnvironment.developmentInMemory =>
-      app_config.AppConfig.developmentInMemory(),
-    app_config.AppEnvironment.developmentApi =>
-      app_config.AppConfig.developmentApi(),
+    app_config.AppEnvironment.demo => app_config.AppConfig.demo(),
+    app_config.AppEnvironment.development => app_config.AppConfig.development(),
   };
   await bootstrap(appConfig);
 }

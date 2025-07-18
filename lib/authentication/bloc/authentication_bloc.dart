be14090ae380168c4f49740588309c5d7f8bb052
fulleni <fulleni@hotmail.com>
2025-07-18 08:44:50 +0100

    refactor(auth): remove unnecessary imports
    - Removed unused imports from `authentication_bloc.dart`.
    - Improved code clarity and maintainability.

--- lib/authentication/bloc/authentication_bloc.dart
+++ lib/authentication/bloc/authentication_bloc.dart
@@ -3,15 +3,7 @@ import 'dart:async';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:ht_auth_repository/ht_auth_repository.dart';
-import 'package:ht_shared/ht_shared.dart'
-    show
-        AuthenticationException,
-        HtHttpException,
-        InvalidInputException,
-        NetworkException,
-        OperationFailedException,
-        ServerException,
-        User;
+import 'package:ht_shared/ht_shared.dart';
 
 part 'authentication_event.dart';
 part 'authentication_state.dart';

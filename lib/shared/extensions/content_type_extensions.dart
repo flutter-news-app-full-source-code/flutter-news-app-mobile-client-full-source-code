617407b7f8b2d30e8d666a05cfb7c0c47caf38c6
fulleni <fulleni@hotmail.com>
2025-07-18 17:38:20 +0100

    feat(l10n): add extension for localized content type names
    - Create ContentTypeX extension to provide displayName method
    - Implement localized display names for all content types
    - Add comments for non-searchable content types

---
+++ lib/shared/extensions/content_type_extensions.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/widgets.dart';
+import 'package:ht_main/l10n/l10n.dart';
+import 'package:ht_shared/ht_shared.dart';
+
+/// An extension on [ContentType] to provide localized display names.
+extension ContentTypeX on ContentType {
+  /// Returns a user-friendly, localized display name for the content type.
+  String displayName(BuildContext context) {
+    switch (this) {
+      case ContentType.headline:
+        return context.l10n.contentTypeHeadline;
+      case ContentType.topic:
+        return context.l10n.contentTypeTopic;
+      case ContentType.source:
+        return context.l10n.contentTypeSource;
+      case ContentType.country:
+        // While not searchable, providing a name is good practice.
+        return context.l10n.contentTypeCountry;
+    }
+  }
+}

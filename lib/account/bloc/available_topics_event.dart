3bde1d7ee5d68a730110af79c079376099cc078c
fulleni <fulleni@hotmail.com>
2025-07-18 06:33:20 +0100

    feat(account): add fetch available topics event
    - Added FetchAvailableTopics event
    - Updated AvailableTopicsEvent to abstract class

--- lib/account/bloc/available_topics_event.dart
+++ lib/account/bloc/available_topics_event.dart
@@ -1,8 +1,12 @@
 part of 'available_topics_bloc.dart';
 
-sealed class AvailableTopicsEvent extends Equatable {
+abstract class AvailableTopicsEvent extends Equatable {
   const AvailableTopicsEvent();
 
   @override
   List<Object> get props => [];
+}
+
+class FetchAvailableTopics extends AvailableTopicsEvent {
+  const FetchAvailableTopics();
 }

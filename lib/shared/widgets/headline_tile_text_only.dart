f438cca91f7d57b8eea2a521376dbf8ead984516
fulleni <fulleni@hotmail.com>
2025-07-21 19:06:43 +0100

    feat(shared): implement timeago package for relative timestamps
    - Add timeago package to dependencies
    - Replace custom date formatting with timeago.format() function

--- lib/shared/widgets/headline_tile_text_only.dart
+++ lib/shared/widgets/headline_tile_text_only.dart
@@ -4,6 +4,7 @@ import 'package:ht_main/entity_details/view/entity_details_page.dart';
 import 'package:ht_main/router/routes.dart';
 import 'package:ht_shared/ht_shared.dart';
 import 'package:ht_ui_kit/ht_ui_kit.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 /// {@template headline_tile_text_only}
 /// A widget to display a headline item with text only.
@@ -108,8 +109,7 @@ class _HeadlineMetadataRow extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // TODO(anyone): Use a proper timeago library.
-    final formattedDate = headline.createdAt.toString();
+    final formattedDate = timeago.format(headline.createdAt);
 
     // Use bodySmall for a reasonable base size, with muted accent color
     final metadataTextStyle = textTheme.bodySmall?.copyWith(

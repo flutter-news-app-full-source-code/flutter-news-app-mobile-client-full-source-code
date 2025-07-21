3dc4112cfeeb053a6602d878b2c3ec985c035ed0
fulleni <fulleni@hotmail.com>
2025-07-21 18:59:24 +0100

    feat(headlines-search): add TopicItemWidget for search results
    - Create TopicItemWidget to display Topic search results
    - Replace temporary ListTile with TopicItemWidget in search results
    - Add navigation to Topic details page on item tap

---
+++ lib/headlines-search/widgets/topic_item_widget.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:ht_main/entity_details/view/entity_details_page.dart';
+import 'package:ht_main/router/routes.dart';
+import 'package:ht_shared/ht_shared.dart';
+
+/// A simple widget to display a Topic search result.
+class TopicItemWidget extends StatelessWidget {
+  const TopicItemWidget({required this.topic, super.key});
+
+  final Topic topic;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(topic.name),
+      subtitle: topic.description.isNotEmpty
+          ? Text(
+              topic.description,
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            )
+          : null,
+      onTap: () {
+        context.push(
+          Routes.topicDetails,
+          extra: EntityDetailsPageArguments(entity: topic),
+        );
+      },
+    );
+  }
+}

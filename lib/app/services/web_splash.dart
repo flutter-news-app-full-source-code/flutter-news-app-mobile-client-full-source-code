d67834521b4a6908f743e48efb30742a47fbc097
fulleni <fulleni@hotmail.com>
2025-07-29 06:39:59 +0100

    refactor(splash): remove unused web splash code and update imports
    - Remove unused JS interop import and external function
    - Remove unused import for DevicePreview
    - Add missing import for WebSplash service

---
+++ lib/app/services/web_splash.dart
@@ -0,0 +1,2 @@
+export 'splash_remover_stub.dart'
+    if (dart.library.html) 'splash_remover_web.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
-import 'dart:js_interop';
-
 import 'package:device_preview/device_preview.dart';
+import 'package:flutter_news_app_mobile_client_full_source_code/app/services/web_splash.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_news_app_mobile_client_full_source_code/app/config/config.dart';
@@ -8,9 +7,6 @@ import 'package:flutter_news_app_mobile_client_full_source_code/bootstrap.dart';
 
 // Define the current application environment (production/development/demo).
 const appEnvironment = AppEnvironment.development;
-
-@JS('removeSplashFromWeb')
-external void removeSplashFromWeb();
 
 void main() async {
   final appConfig = switch (appEnvironment) {

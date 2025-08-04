8cdad523a17158a792e07be0f14ee7b164a90442
fulleni <fulleni@hotmail.com>
2025-08-04 17:24:15 +0100

    fix(headlines-feed): update topic sorting order to ascending
    - Change the sort order from descending to ascending for both
    initial data load and subsequent fetches
    - This ensures topics are displayed in alphabetical order

--- lib/headlines-feed/bloc/topics_filter_bloc.dart
+++ lib/headlines-feed/bloc/topics_filter_bloc.dart
@@ -54,7 +54,7 @@ class TopicsFilterBloc extends Bloc<TopicsFilterEvent, TopicsFilterState> {
     try {
       final response = await _topicsRepository.readAll(
         pagination: const PaginationOptions(limit: _topicsLimit),
-        sort: [const SortOption('name', SortOrder.desc)],
+        sort: [const SortOption('name', SortOrder.asc)],
       );
       emit(
         state.copyWith(
@@ -88,7 +88,7 @@ class TopicsFilterBloc extends Bloc<TopicsFilterEvent, TopicsFilterState> {
           limit: _topicsLimit,
           cursor: state.cursor,
         ),
-        sort: [const SortOption('name', SortOrder.desc)],
+        sort: [const SortOption('name', SortOrder.asc)],
       );
       emit(
         state.copyWith(

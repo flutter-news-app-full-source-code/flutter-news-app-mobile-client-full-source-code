25160ef051add6d60471648b476c148fe5d5cc9e
fulleni <fulleni@hotmail.com>
2025-08-16 16:46:49 +0100

    fix(shared): update import path for FeedCore widget
    - Update import statement in entity_details_page.dart
    - Update import statement in headline_details_page.dart
    - Change shared.dart import to direct feed_core.dart import in both files

--- lib/headline-details/view/headline_details_page.dart
+++ lib/headline-details/view/headline_details_page.dart
@@ -12,7 +12,7 @@ import 'package:flutter_news_app_mobile_client_full_source_code/headline-details
 import 'package:flutter_news_app_mobile_client_full_source_code/headline-details/bloc/similar_headlines_bloc.dart';
 import 'package:flutter_news_app_mobile_client_full_source_code/l10n/l10n.dart';
 import 'package:flutter_news_app_mobile_client_full_source_code/router/routes.dart';
-import 'package:flutter_news_app_mobile_client_full_source_code/shared/shared.dart';
+import 'package:flutter_news_app_mobile_client_full_source_code/shared/widgets/feed_core/feed_core.dart';
 import 'package:go_router/go_router.dart';
 import 'package:intl/intl.dart';
 import 'package:share_plus/share_plus.dart';

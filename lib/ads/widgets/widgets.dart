fb1f65a5a00fcbcd9ff9860532b9ca39be6b6707
fulleni <fulleni@hotmail.com>
2025-09-05 10:08:06 +0100

    refactor(ads): rename AdmobNativeAdWidget to AdmobInlineAdWidget
    - Update class name from AdmobNativeAdWidget to AdmobInlineAdWidget
    - Rename corresponding file from admob_native_ad_widget.dart to admob_inline_ad_widget.dart

--- lib/ads/widgets/widgets.dart
+++ lib/ads/widgets/widgets.dart
@@ -1,6 +1,6 @@
 export 'ad_feed_item_widget.dart';
 export 'ad_loader_widget.dart';
-export 'admob_native_ad_widget.dart';
+export 'admob_inline_ad_widget.dart';
 export 'local_banner_ad_widget.dart';
 export 'local_native_ad_widget.dart';
 export 'placeholder_ad_widget.dart';

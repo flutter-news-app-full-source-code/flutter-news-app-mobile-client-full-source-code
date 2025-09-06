f49083d5dac2403452f98f31b3953858b77b8172
fulleni <fulleni@hotmail.com>
2025-09-06 08:02:57 +0100

    chore: barrels

--- lib/ads/models/models.dart
+++ lib/ads/models/models.dart
@@ -1,5 +1,7 @@
 export 'ad_feed_item.dart';
 export 'ad_placeholder.dart';
 export 'ad_theme_style.dart';
+export 'banner_ad.dart';
+export 'inline_ad.dart';
 export 'interstitial_ad.dart';
 export 'native_ad.dart';
--- lib/ads/widgets/widgets.dart
+++ lib/ads/widgets/widgets.dart
@@ -1,6 +1,7 @@
 export 'ad_feed_item_widget.dart';
 export 'ad_loader_widget.dart';
 export 'admob_inline_ad_widget.dart';
+export 'in_article_ad_loader_widget.dart';
 export 'local_banner_ad_widget.dart';
 export 'local_native_ad_widget.dart';
 export 'placeholder_ad_widget.dart';

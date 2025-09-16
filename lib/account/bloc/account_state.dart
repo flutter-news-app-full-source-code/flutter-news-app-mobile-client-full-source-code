f5fc7dc6308ec643351ff81740564206ee4fbdc0
fulleni <fulleni@hotmail.com>
2025-09-16 18:22:17 +0100

    refactor(account): remove preferences from AccountState
    - Remove UserContentPreferences from AccountState
    - Update copyWith method to reflect the removal
    - Adjust props list to exclude preferences

--- lib/account/bloc/account_state.dart
+++ lib/account/bloc/account_state.dart
@@ -6,32 +6,27 @@ class AccountState extends Equatable {
   const AccountState({
     this.status = AccountStatus.initial,
     this.user,
-    this.preferences,
     this.error,
   });
 
   final AccountStatus status;
   final User? user;
-  final UserContentPreferences? preferences;
   final HttpException? error;
 
   AccountState copyWith({
     AccountStatus? status,
     User? user,
-    UserContentPreferences? preferences,
     HttpException? error,
     bool clearUser = false,
-    bool clearPreferences = false,
     bool clearError = false,
   }) {
     return AccountState(
       status: status ?? this.status,
       user: clearUser ? null : user ?? this.user,
-      preferences: clearPreferences ? null : preferences ?? this.preferences,
       error: clearError ? null : error ?? this.error,
     );
   }
 
   @override
-  List<Object?> get props => [status, user, preferences, error];
+  List<Object?> get props => [status, user, error];
 }

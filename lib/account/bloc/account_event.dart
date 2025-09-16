6ee2e6981f3d58b709919569384c787a1f348714
fulleni <fulleni@hotmail.com>
2025-09-16 18:22:37 +0100

    refactor(account): remove unused account events
    - Removed AccountLoadUserPreferences, AccountSaveHeadlineToggled, AccountFollowTopicToggled, AccountFollowSourceToggled, and AccountFollowCountryToggled events
    - These events were likely moved to the preferences feature

--- lib/account/bloc/account_event.dart
+++ lib/account/bloc/account_event.dart
@@ -16,46 +16,6 @@ class AccountUserChanged extends AccountEvent {
   List<Object?> get props => [user];
 }
 
-class AccountLoadUserPreferences extends AccountEvent {
-  // Corrected name
-  const AccountLoadUserPreferences({required this.userId});
-  final String userId;
-
-  @override
-  List<Object> get props => [userId];
-}
-
-class AccountSaveHeadlineToggled extends AccountEvent {
-  const AccountSaveHeadlineToggled({required this.headline});
-  final Headline headline;
-
-  @override
-  List<Object> get props => [headline];
-}
-
-class AccountFollowTopicToggled extends AccountEvent {
-  const AccountFollowTopicToggled({required this.topic});
-  final Topic topic;
-
-  @override
-  List<Object> get props => [topic];
-}
-
-class AccountFollowSourceToggled extends AccountEvent {
-  const AccountFollowSourceToggled({required this.source});
-  final Source source;
-
-  @override
-  List<Object> get props => [source];
-}
-
-class AccountFollowCountryToggled extends AccountEvent {
-  const AccountFollowCountryToggled({required this.country});
-  final Country country;
-
-  @override
-  List<Object> get props => [country];
-}
 
 class AccountClearUserPreferences extends AccountEvent {
   const AccountClearUserPreferences({required this.userId});

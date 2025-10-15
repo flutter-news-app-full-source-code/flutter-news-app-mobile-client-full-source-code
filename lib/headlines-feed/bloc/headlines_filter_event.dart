3f1501c815d6b06b1aa82f745198da974ae60fe6
fulleni <fulleni@hotmail.com>
2025-10-15 10:03:14 +0100

    refactor(feed): remove obsolete followed-item filter events
    Removes the `isUsingFollowedItems` property from the `FilterDataLoaded` event and deletes the `FollowedTopicsFilterToggled`, `FollowedSourcesFilterToggled`, and `FollowedCountriesFilterToggled` events.
    
    This is part of a larger refactoring to replace the ambiguous "apply followed" flag with a more explicit and robust "Followed" filter managed by the `HeadlinesFeedBloc`.

--- lib/headlines-feed/bloc/headlines_filter_event.dart
+++ lib/headlines-feed/bloc/headlines_filter_event.dart
@@ -22,7 +22,6 @@ final class FilterDataLoaded extends HeadlinesFilterEvent {
     this.initialSelectedTopics = const [],
     this.initialSelectedSources = const [],
     this.initialSelectedCountries = const [],
-    this.isUsingFollowedItems = false,
   });
 
   /// The topics that were initially selected on the previous page.
@@ -34,15 +33,11 @@ final class FilterDataLoaded extends HeadlinesFilterEvent {
   /// The countries that were initially selected on the previous page.
   final List<Country> initialSelectedCountries;
 
-  /// Whether the filter is initially set to use followed items.
-  final bool isUsingFollowedItems;
-
   @override
   List<Object> get props => [
     initialSelectedTopics,
     initialSelectedSources,
     initialSelectedCountries,
-    isUsingFollowedItems,
   ];
 }
 
@@ -103,46 +98,4 @@ final class FilterCountryToggled extends HeadlinesFilterEvent {
 final class FilterSelectionsCleared extends HeadlinesFilterEvent {
   /// {@macro filter_selections_cleared}
   const FilterSelectionsCleared();
-}
-
-/// {@template followed_topics_filter_toggled}
-/// Event triggered when the "Apply my followed topics" button is toggled.
-/// {@endtemplate}
-final class FollowedTopicsFilterToggled extends HeadlinesFilterEvent {
-  /// {@macro followed_topics_filter_toggled}
-  const FollowedTopicsFilterToggled({required this.isSelected});
-
-  /// The new selection state of the followed topics filter.
-  final bool isSelected;
-
-  @override
-  List<Object> get props => [isSelected];
-}
-
-/// {@template followed_sources_filter_toggled}
-/// Event triggered when the "Apply my followed sources" button is toggled.
-/// {@endtemplate}
-final class FollowedSourcesFilterToggled extends HeadlinesFilterEvent {
-  /// {@macro followed_sources_filter_toggled}
-  const FollowedSourcesFilterToggled({required this.isSelected});
-
-  /// The new selection state of the followed sources filter.
-  final bool isSelected;
-
-  @override
-  List<Object> get props => [isSelected];
-}
-
-/// {@template followed_countries_filter_toggled}
-/// Event triggered when the "Apply my followed countries" button is toggled.
-/// {@endtemplate}
-final class FollowedCountriesFilterToggled extends HeadlinesFilterEvent {
-  /// {@macro followed_countries_filter_toggled}
-  const FollowedCountriesFilterToggled({required this.isSelected});
-
-  /// The new selection state of the followed countries filter.
-  final bool isSelected;
-
-  @override
-  List<Object> get props => [isSelected];
 }

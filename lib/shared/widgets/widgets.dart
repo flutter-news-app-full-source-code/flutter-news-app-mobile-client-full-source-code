d5a89be172bb9519ff656326760c116a42f92d51
fulleni <fulleni@hotmail.com>
2025-10-27 07:38:35 +0000

    refactor(feed_decorators): reorganize widget and service locations
    - Move feed decorator widgets from shared/widgets/feed_decorators to feed_decorators/widgets
    - Remove FeedDecoratorService from shared/services
    - Update related exports in services.dart and feed_decorators.dart

--- lib/shared/widgets/widgets.dart
+++ lib/shared/widgets/widgets.dart
@@ -1,6 +1,5 @@
 export 'confirmation_dialog.dart';
 export 'content_limitation_bottom_sheet.dart';
 export 'feed_core/feed_core.dart';
-export 'feed_decorators/feed_decorators.dart';
 export 'multi_select_search_page.dart';
 export 'user_avatar.dart';

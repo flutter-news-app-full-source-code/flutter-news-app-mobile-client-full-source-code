f89a4d5b78b233458deb3d12c19fad9d2bba6696
fulleni <fulleni@hotmail.com>
2025-11-19 10:41:28 +0100

    refactor(notifications): simulate permission request in NoOpPushNotificationService
    - Add ValueNotifier to simulate permission state
    - Implement requestPermission to update permission state
    - Modify hasPermission to reflect simulated permission state

--- lib/notifications/services/no_op_push_notification_service.dart
+++ lib/notifications/services/no_op_push_notification_service.dart
@@ -1,4 +1,5 @@
 import 'package:core/core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter_news_app_mobile_client_full_source_code/notifications/services/push_notification_service.dart';
 
 /// {@template no_op_push_notification_service}
@@ -10,14 +11,26 @@ import 'package:flutter_news_app_mobile_client_full_source_code/notifications/se
 /// empty implementations for all required methods.
 /// {@endtemplate}
 class NoOpPushNotificationService extends PushNotificationService {
+  final ValueNotifier<bool> _permissionState = ValueNotifier(false);
+
   @override
   Future<void> initialize() async {}
 
   @override
-  Future<bool> requestPermission() async => true;
+  Future<bool> requestPermission() async {
+    // Simulate the user granting permission.
+    _permissionState.value = true;
+    return Future.value(true);
+  }
 
   @override
-  Future<bool> hasPermission() async => true;
+  Future<bool> hasPermission() async {
+    // In the demo environment, this simulates the permission state.
+    // It starts as `false` and becomes `true` after `requestPermission` is
+    // called, allowing the UI to show a pre-permission dialog on the first
+    // attempt.
+    return Future.value(_permissionState.value);
+  }
 
   @override
   Future<void> registerDevice({required String userId}) async {}

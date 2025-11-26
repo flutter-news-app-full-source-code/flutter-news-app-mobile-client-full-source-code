bf193997486c85e3c2b17715a2beab65c59b3511
fulleni <fulleni@hotmail.com>
2025-11-26 08:41:32 +0100

    style: remove unnecessary hide directive in import
    - Remove 'hide AppStatus' directive from core package import
    - This change simplifies the import statement and removes unnecessary hiding of AppStatus

--- lib/account/view/account_page.dart
+++ lib/account/view/account_page.dart
@@ -1,4 +1,4 @@
-import 'package:core/core.dart' hide AppStatus;
+import 'package:core/core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_news_app_mobile_client_full_source_code/app/bloc/app_bloc.dart';

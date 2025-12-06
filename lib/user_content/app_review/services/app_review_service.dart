7dae83bdad140f850da721dc5f5887a105960fe6
fulleni <fulleni@hotmail.com>
2025-12-06 06:31:39 +0100

    feat(app_review): add environment-aware initial prompt cooldown
    - Introduce a check for the app environment before considering the initial prompt cooldown
    - This allows ignoring the cooldown period in the demo environment, speeding up the review process for testers
    - The change maintains the existing behavior for other environments

--- lib/user_content/app_review/services/app_review_service.dart
+++ lib/user_content/app_review/services/app_review_service.dart
@@ -5,6 +5,7 @@ import 'package:data_repository/data_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_news_app_mobile_client_full_source_code/app/bloc/app_bloc.dart';
+import 'package:flutter_news_app_mobile_client_full_source_code/app/config/app_environment.dart';
 import 'package:flutter_news_app_mobile_client_full_source_code/user_content/app_review/services/native_review_service.dart';
 import 'package:flutter_news_app_mobile_client_full_source_code/user_content/app_review/view/provide_feedback_bottom_sheet.dart';
 import 'package:flutter_news_app_mobile_client_full_source_code/user_content/app_review/view/rate_app_bottom_sheet.dart';
@@ -41,6 +42,7 @@ class AppReviewService {
   }) async {
     final appState = context.read<AppBloc>().state;
     final user = appState.user;
+    final environment = context.read<AppEnvironment>();
     final remoteConfig = appState.remoteConfig;
 
     if (user == null || remoteConfig == null) {
@@ -65,11 +67,14 @@ class AppReviewService {
 
     // Check initial cooldown.
     final daysSinceCreation = DateTime.now().difference(user.createdAt).inDays;
-    if (daysSinceCreation < appReviewConfig.initialPromptCooldownDays) {
-      _logger.fine(
-        'User is within the initial cooldown period ($daysSinceCreation/${appReviewConfig.initialPromptCooldownDays} days).',
-      );
-      return;
+    // For the demo environment, we ignore the initial cooldown period.
+    if (environment != AppEnvironment.demo) {
+      if (daysSinceCreation < appReviewConfig.initialPromptCooldownDays) {
+        _logger.fine(
+          'User is within the initial cooldown period ($daysSinceCreation/${appReviewConfig.initialPromptCooldownDays} days).',
+        );
+        return;
+      }
     }
 
     // Check positive interaction threshold.

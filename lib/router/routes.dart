23370fbb9bec012a4b66c49b3b90dc6c3fa6ddb6
fulleni <fulleni@hotmail.com>
2025-12-27 14:51:55 +0100

    refactor(router): reorder static const variables in Routes class
    - Move subscriptionDetails and subscriptionDetailsName below notificationsCenter and notificationsCenterName
    - This reordering improves the logical grouping of related route constants

--- lib/router/routes.dart
+++ lib/router/routes.dart
@@ -21,8 +21,6 @@ abstract final class Routes {
   static const accountName = 'account';
   static const discover = '/discover';
   static const discoverName = 'discover';
-  static const subscriptionDetails = 'subscription-details';
-  static const subscriptionDetailsName = 'subscriptionDetails';
   static const paywall = '/paywall';
   static const paywallName = 'paywall';
 
@@ -39,6 +37,8 @@ abstract final class Routes {
   static const manageFollowedItemsName = 'manageFollowedItems';
   static const notificationsCenter = 'notifications-center';
   static const notificationsCenterName = 'notificationsCenter';
+  static const subscriptionDetails = 'subscription-details';
+  static const subscriptionDetailsName = 'subscriptionDetails';
 
   // --- Relative Sub-Routes ---
   // These routes are defined with relative paths and are intended to be

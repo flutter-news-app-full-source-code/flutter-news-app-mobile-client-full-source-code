b065641fd0844ba6194d47d9c847682b97ee8694
fulleni <fulleni@hotmail.com>
2026-01-02 07:40:25 +0100

    chore: delete absolete files

--- lib/ads/providers/providers.dart
+++ lib/ads/providers/providers.dart
@@ -1,3 +1,2 @@
 export 'ad_provider.dart';
 export 'admob_ad_provider.dart';
-export 'demo_ad_provider.dart';
--- lib/ads/widgets/widgets.dart
+++ lib/ads/widgets/widgets.dart
@@ -1,5 +1,2 @@
 export 'admob_inline_ad_widget.dart';
-export 'demo_banner_ad_widget.dart';
-export 'demo_interstitial_ad_dialog.dart';
-export 'demo_native_ad_widget.dart';
 export 'feed_ad_loader_widget.dart';

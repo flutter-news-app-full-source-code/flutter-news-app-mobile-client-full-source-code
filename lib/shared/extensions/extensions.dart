5d25e780c911100304cb302c5809dedc4f8d6cc2
Fulleni Flen <fulleni@hotmail.com>
2026-03-01 08:40:09 +0100

    fix: typos

--- lib/shared/extensions/extensions.dart
+++ lib/shared/extensions/extensions.dart
@@ -2,6 +2,6 @@ export 'app_status_x.dart';
 export 'content_type_extensions.dart';
 export 'multilingual_map_extension.dart';
 export 'source_type_l10n_extensions.dart';
-export 'supported_language_flag.dart.dart';
-export 'supported_language_l10n.dart.dart';
+export 'supported_language_flag.dart';
+export 'supported_language_l10n.dart';
 export 'user_feed_decorator_status_x.dart';
